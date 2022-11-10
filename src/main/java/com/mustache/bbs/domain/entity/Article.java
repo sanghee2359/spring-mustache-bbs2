5cd90b5602d64dd6a061b036de9f0c4d7be1543e
sanghee <wjdtkdgml7352@daum.net>
2022-11-10 11:24:52 +0900

    [추가] entity 생성

---
+++ src/main/java/com/mustache/bbs/domain/entity/Article.java
@@ -0,0 +1,21 @@
+package com.mustache.bbs.domain.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="article4")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+}
