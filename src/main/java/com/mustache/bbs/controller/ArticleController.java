aa2ec3c5761580cb2197e4155d5b4b42f8408f07
sanghee <wjdtkdgml7352@daum.net>
2022-11-10 13:08:16 +0900

    [추가] Controller - findById메소드

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -3,10 +3,16 @@ package com.mustache.bbs.controller;
 import com.mustache.bbs.domain.dto.ArticleDto;
 import com.mustache.bbs.domain.entity.Article;
 import com.mustache.bbs.repository.ArticleReapository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping(value = "/articles")
@@ -19,7 +25,7 @@ public class ArticleController {
 
     @GetMapping(value = "/new")
     public String formPage(){
-        return "articles/new";
+        return "new";
     }
     @PostMapping("")
     public String createArticle(ArticleDto articleDto){
@@ -28,4 +34,13 @@ public class ArticleController {
         Article saved = articleReapository.save(article);
         return String.format("redirect:/articles/%d",saved.getId());
     }
+    @GetMapping(value="/{id}")
+    public String getArticle(@PathVariable Long id, Model model){
+        Optional<Article> optArticle = articleReapository.findById(id);
+        if(!optArticle.isEmpty()){
+            model.addAttribute("article",optArticle.get());
+            return "show";
+        }else return "error";
+    }
+
 }
