5b5032a5eee8a42464712a71915adad944e8d68d
sanghee <wjdtkdgml7352@daum.net>
2022-11-10 11:30:38 +0900

    [수정] GeneratedValue 설정

--- src/main/java/com/mustache/bbs/domain/entity/Article.java
+++ src/main/java/com/mustache/bbs/domain/entity/Article.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
 @Getter
 public class Article {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String content;
