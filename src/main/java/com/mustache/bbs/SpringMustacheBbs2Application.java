6191be6976491498538a444b3b6181dcf3547390
sanghee <wjdtkdgml7352@daum.net>
2022-11-10 11:06:38 +0900

    First Commit

---
+++ src/main/java/com/mustache/bbs/SpringMustacheBbs2Application.java
@@ -0,0 +1,13 @@
+package com.mustache.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMustacheBbs2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMustacheBbs2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/mustache/bbs/SpringMustacheBbs2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustache.bbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMustacheBbs2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
