cb80f181fb4009b2ce2c69c4aed7d23d5639bbc0
sanghee <wjdtkdgml7352@daum.net>
2022-11-14 13:59:44 +0900

    [추가] Repository에 메소드 테스트 코드 - 보건소, 보건지소 찾기

--- src/test/java/com/mustache/bbs/repository/HospitalRepositoryTest.java
+++ src/test/java/com/mustache/bbs/repository/HospitalRepositoryTest.java
@@ -1,10 +1,13 @@
 package com.mustache.bbs.repository;
 
 import com.mustache.bbs.domain.entity.Hospital;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,6 +15,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class HospitalRepositoryTest {
     @Autowired
     HospitalRepository hospitalRepository;
+
+    @Test
+    @DisplayName("BusinessTypeName이 보건소 보건지소 보건진료소인 데이터가 잘 나오는지")
+    void findByBusinessTypeNameIn() {
+        List<String> inClues = new ArrayList<>();
+        inClues.add("보건소");
+        inClues.add("보건지소");
+
+        inClues.add("보건진료소");
+        List<Hospital> hospitals = hospitalRepository.findByBussinessTypeNameIn(inClues);
+        for (var hospital :
+                hospitals) {
+            System.out.println(hospital.getHospitalName());
+        }
+    }
+
 
     @Test
     void name(){
