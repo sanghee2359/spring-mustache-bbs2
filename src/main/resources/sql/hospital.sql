636bd832882bddc9ae45482b06f091d51fa7fc85
sanghee <wjdtkdgml7352@daum.net>
2022-11-14 13:57:37 +0900

    [완성]sql문 작성 - 특정 구의 보건소, 보건지소 찾기

--- src/main/resources/sql/hospital.sql
+++ src/main/resources/sql/hospital.sql
@@ -1,1 +1,12 @@
+/*
+ 경기도 수원시에 있는 피부과 출력하기
+ */
 SELECT hospital_name, road_name_address FROM `like-lion-db`.nation_wide_hospital WHERE road_name_address LIKE '경기도 수원시%' and hospital_name LIKE '%피부과%';
+
+/*
+ 특정 구의 보건소, 보건지소 찾기
+ */
+SELECT business_type_name, hospital_name, road_name_address
+FROM `likelion-db`.nation_wide_hospitals
+where business_type_name in ('보건소', '보건지소')
+;
