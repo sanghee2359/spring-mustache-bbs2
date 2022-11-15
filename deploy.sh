fd4e41176a7f744519cf15d3f702f9f20637b5da
sanghee <wjdtkdgml7352@daum.net>
2022-11-15 14:31:03 +0900

    [추가] deploy script 초안

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+# Build N Run Container with DB Container
+# 2020. 09. 18 Zini
+
+### You MUST RUN Docker Container First ###
+SPRING_DATASOURCE_URL=""
+SPRING_DATASOURCE_PASSWORD=""
+project_name="spring-mustache-bbs2"
+container_id=""
+image_name="spring-mustache-bbs"
+host_port=8080
+virtual_port=8080
+
+echo "## Automation docker build and run ##"
+
+# remove container
+echo "=> Stop previous container..."
+docker stop ${container_id}
+
+# move image
+echo "=> Remove previous image..."
+cd ${project_name}
+
+# git pull
+git pull
+
+# new-build/re-build docker image
+echo "=> Build new image..."
+docker build -t ${image_name} .
+
+# Run container connected to existing docker container
+echo "=> Run container..."
+docker run -p ${host_port}:${virtual_port} -e SPRING_DATASOURCE_URL=${SPRING_DATASOURCE_URL} -e SPRING_DATASOURCE_PASSWORD=${SPRING_DATASOURCE_PASSWORD} ${image_name}
