7e49a4cbec91bb4b7df394ae26ba495c66df61f0
sanghee <wjdtkdgml7352@daum.net>
2022-11-15 13:53:24 +0900

    [추가] HospitalResponse의 생성자, setBusinessStatusName 작성

--- src/main/java/com/mustache/bbs/domain/dto/HospitalResponse.java
+++ src/main/java/com/mustache/bbs/domain/dto/HospitalResponse.java
@@ -18,8 +18,12 @@ public class HospitalResponse {
     private Integer totalNumberOfBeds;
     private String businessTypeName;
     private Float totalAreaSize;
+    private String businessStatusName;
 
 
-   /* public HospitalResponse(Integer id, String roadNameAddress, String hospitalName, Integer patientRoomCount, Integer totalNumberOfBeds, String businessTypeName, Float totalAreaSize) {
-    }*/
+    public HospitalResponse(Integer id, String roadNameAddress, String hospitalName, Integer patientRoomCount, Integer totalNumberOfBeds, String businessTypeName, Float totalAreaSize) {
+    }
+    public void setBusinessStatusName(String businessStatusName) {
+        this.businessStatusName = businessStatusName;
+    }
 }
