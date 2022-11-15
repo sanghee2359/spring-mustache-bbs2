ad65357ee2514163405af0d9eac3b7e4484b9dbf
sanghee <wjdtkdgml7352@daum.net>
2022-11-15 14:17:00 +0900

    [수정] HospitalResponse 생성자 초기화

--- src/main/java/com/mustache/bbs/domain/dto/HospitalResponse.java
+++ src/main/java/com/mustache/bbs/domain/dto/HospitalResponse.java
@@ -22,6 +22,13 @@ public class HospitalResponse {
 
 
     public HospitalResponse(Integer id, String roadNameAddress, String hospitalName, Integer patientRoomCount, Integer totalNumberOfBeds, String businessTypeName, Float totalAreaSize) {
+        this.id = id;
+        this.roadNameAddress = roadNameAddress;
+        this.hospitalName = hospitalName;
+        this.patientRoomCount = patientRoomCount;
+        this.totalNumberOfBeds = totalNumberOfBeds;
+        this.businessTypeName = businessTypeName;
+        this.totalAreaSize = totalAreaSize;
     }
     public void setBusinessStatusName(String businessStatusName) {
         this.businessStatusName = businessStatusName;
