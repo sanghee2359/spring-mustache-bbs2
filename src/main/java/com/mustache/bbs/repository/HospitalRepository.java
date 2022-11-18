fb4cd0d85c8ab757e0a68b832fda2ab49addf21b
sanghee <wjdtkdgml7352@daum.net>
2022-11-18 15:03:23 +0900

    [추가] 특정 도로명주소를 포함하는 method Query 생성

--- src/main/java/com/mustache/bbs/repository/HospitalRepository.java
+++ src/main/java/com/mustache/bbs/repository/HospitalRepository.java
@@ -1,12 +1,15 @@
 package com.mustache.bbs.repository;
 
 import com.mustache.bbs.domain.entity.Hospital;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 
 public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
+    Page<Hospital> findByRoadNameAddressContaining(String str, Pageable pageable);
     List<Hospital> findByBusinessTypeNameIn(List<String> businessTypes);
 
     List<Hospital> findByRoadNameAddressContaining(String keyword); // 포함
